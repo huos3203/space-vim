8814f2a1a1c3a720cc49a0b383530cf6d4ff96dc
huos3203 <724987481@qq.com>
2017-06-30 08:13:15 +0800

    配置space-vim安装脚本

--- Manualinstall.sh
+++ Manualinstall.sh
@@ -12,13 +12,13 @@ echo "已经存在"
 exit 0
 fi
 
-#vimdir=`pwd`/Util/space-vim
-##目录替身
-#ln -fs $vimdir ~/.space-vim
-## for vim
-#ln -fs $vimdir/init.vim ~/.vimrc
-#ln -fs $vimdir/spacevim ~/.spacevim
-## for neovim
-#mkdir -p ~/.config/nvim
-#ln -fs $vimdir/init.vim ~/.config/nvim/init.vim
+vimdir=`pwd`/Util/space-vim
+#目录替身
+ln -fs $vimdir ~/.space-vim
+# for vim
+ln -fs $vimdir/init.vim ~/.vimrc
+ln -fs $vimdir/spacevim ~/.spacevim
+# for neovim
+mkdir -p ~/.config/nvim
+ln -fs $vimdir/init.vim ~/.config/nvim/init.vim
 
