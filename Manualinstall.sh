337cbd372d12b0379333de097760bf939c7398ce
ddd <724987481@qq.com>
2017-06-30 11:55:59 +0800

    space-vim安装脚本优化，使用函数式创建多个文件的替身

--- Manualinstall.sh
+++ Manualinstall.sh
@@ -6,19 +6,34 @@
 #  Created by huoshuguang on 2017/6/30.
 #  Copyright © 2017年 boyer. All rights reserved.
 pwd
+vimdir=`pwd`/Util/space-vim
 spacehome=~/.space-vim
 if [ -d "$spacehome" ]; then
-echo "已经存在"
-exit 0
+    echo "已经存在"
+else
+    #目录替身
+    ln -fs $vimdir ~/.space-vim
 fi
 
-vimdir=`pwd`/Util/space-vim
-#目录替身
-ln -fs $vimdir ~/.space-vim
+function lnfsToFile()
+{
+    lnfile=~/$2
+    if [ -f $lnfile ]
+    then
+        echo $lnfile"已存在"
+    else
+        #制作替身
+        file=$vimdir"/"$1
+        echo "ln -fs $file $lnfile"
+        ln -fs $file $lnfile
+    fi
+}
+
 # for vim
-ln -fs $vimdir/init.vim ~/.vimrc
-ln -fs $vimdir/spacevim ~/.spacevim
+lnfsToFile "init.vim" ".vimrc"
+lnfsToFile "spacevim" ".spacevim"
 # for neovim
 mkdir -p ~/.config/nvim
-ln -fs $vimdir/init.vim ~/.config/nvim/init.vim
+lnfsToFile "init.vim" "/.config/nvim/init.vim"
+
 
